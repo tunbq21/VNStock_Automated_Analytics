efbfd3ecf4569bf557e4a83dbf8dc620719512d8
MokaMoka77 <tbuiquang103@gmail.com>
2026-01-08 17:30:17 +0700

    update dag file

--- dags/vnstock_api_funtion.py
+++ dags/vnstock_api_funtion.py
@@ -1,9 +1,21 @@
 from vnstock import Vnstock
+import pandas as pd
+import os
 
-stock_api = Vnstock()
+# stock_api = Vnstock()
 
-df = stock_api.stock("FPT", "VCI").trading.price_board(
-    symbols_list=["FPT", "HPG", "VCB"]
-)
+# df = stock_api.stock("FPT", "VCI").trading.price_board(
+#     symbols_list=["FPT", "HPG", "VCB"]
+# )
 
-print(df.columns)
+# print(df.columns)
+
+curent_file = os.path.basename(__file__)
+print(f"Đây là tên file: {curent_file}")
+
+
+curent_file_link=os.path.abspath(__file__)
+print(f"Đây là đường dẫn đầy đủ của file: {curent_file_link}")
+
+curent_file_link =os.getcwd()
+print(f"Đây là thư mục hiện tại của file: {curent_file_link}")
